393b495522338d56735e351f4cadd5e37890736e
Athos <athosmrfonseca@gmail.com>
2023-05-20 22:39:54 -0300

    adicionando o slice

---
+++ slice.py
@@ -0,0 +1,14 @@
+
+random = 'errorRANDOM'
+#index = '01234567891011
+
+valor = 1.05
+#aaaa = 0123
+
+valor = str(valor)
+
+#Operaçao SLICE, cortar o texto a partir de sua posiçao numerica
+
+print(random[5:11]) # entre 5 e 11
+print(valor[-1]) # de tras pra frente
+print(valor[:3]) # de qualquer um ate o numero 3
