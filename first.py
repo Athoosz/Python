0b72373ad16eee0b6a3ed39e2f6b77e17b7e4542
Athos <athosmrfonseca@gmail.com>
2023-05-20 22:23:41 -0300

    começando os codigos

--- first.py
+++ first.py
@@ -1,1 +1,20 @@
-teste
+'''
+Python
+'''
+# Primeiro Codigo
+
+# input maneira de inserir algo na variavel
+
+nome = input('Qual o seu nome ? : ')
+x = input('Qual o primeiro numero ? : ')
+y = input('Qual o segundo numero ? : ')
+
+''' Para juntar string com numeros, o python precisa de converter o num em string para isso use str()
+ou int() para converter em int ou float() e etc...
+'''
+
+z = int(x) + int(y)
+
+print('Olá '+ nome)
+print('Resultado da soma é de : ' + str(z))
+
